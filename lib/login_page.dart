bf51269113f78d5ef0d07ee0ac39118ba2348847
Matthew Palkimas <mxp1127@miami.edu>
2020-02-11 23:14:27 -0500

    Reverted back to before without the 2 rs.

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -5,7 +5,7 @@ class LoginPage extends StatelessWidget {
     Widget build(BuildContext context) {
       return new Scaffold(
         appBar: new AppBar(
-          title: new Text('Food rrSavior Login'),
+          title: new Text('Food Savior Login'),
         ),
         body: new Container(
           child: new Text('Hello World')
