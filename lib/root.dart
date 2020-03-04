42eebfd02d1f32ae14d859955fe105b79b5e8a30
Vignesh Selvaraj <vxs377@miami.edu>
2020-03-04 17:13:38 -0500

    Updated root

--- lib/root.dart
+++ lib/root.dart
@@ -32,12 +32,19 @@ class _RootPageState extends State<RootPage>{
   }
 
 
+void _signedIn() {
+  setState((){
+    authStatus = AuthStatus.signedIn;
+  });
+}
+
   @override 
   Widget build(BuildContext context){
     switch(authStatus){
       case AuthStatus.notSignedIn:
         return new LoginPage(
           auth: widget.auth,
+          onSignedIn: _signedIn ,
         );
       case AuthStatus.signedIn:
        return new Scaffold(
