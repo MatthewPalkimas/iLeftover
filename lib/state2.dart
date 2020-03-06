b411b49cc47cf13905f166ed9b59cb029bf53bfa
Vignesh Selvaraj <vxs377@miami.edu>
2020-03-06 03:58:40 -0500

    Database support added, but cannot update or dis

--- lib/state2.dart
+++ lib/state2.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Page2 extends StatefulWidget {
   Page2({this.auth, this.onSignedOut, this.goBack});
@@ -38,29 +39,18 @@ class _Page2PageState extends State<Page2>{
               )
             ],
           ),
-          body: new Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage('Assets/wood-texture.jpg'),
-              fit: BoxFit.cover,
-              ),
-            ),
-            child: new Container(
-              child: new Center(
-                child: new Column(
-                  children: <Widget>[
-                    new Padding(padding: EdgeInsets.only(top: 140.0)),
-                      new Text(
-                         'Yoink Page',
-                          style: new TextStyle(color: Colors.green, fontSize: 25.0),
-                      ),
-                      new Padding(
-                         padding: EdgeInsets.only(top: 50.0)
-                      ),
-                    ],
-                  )
-              ),
-            )
-          ),
-      );
+          body:StreamBuilder(
+            stream: Firestore.instance.collection('Foodinfo').document('food').snapshots(),
+            builder: (context,snapshot) {
+              if(!snapshot.hasData) 
+              return Text('Loading data... Please wait...');
+              return Column(
+                children: <Widget> [
+                  Text(snapshot.data.documents[0]['FoodName']),
+                  Text(snapshot.data.documents[0]['FoodType']) 
+                ],
+              );
+            },
+          ));
     }
 }
