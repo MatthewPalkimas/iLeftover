03e415db51d98bcdb85501435c07e8a93c42becd
Matthew Palkimas <mxp1127@miami.edu>
2020-03-05 23:25:41 -0500

    added some ui to home page

--- lib/donation.dart
+++ lib/donation.dart
@@ -58,33 +58,86 @@ class _HomePageState extends State<HomePage>{
               ),
             ),
             child: new Container(
-              child: new Center(
-                child: new Column(
-                  children: <Widget>[
-                    new Padding(padding: EdgeInsets.only(top: 140.0)),
-                      new Text(
-                         'Welcome motha fuckas',
-                          style: new TextStyle(color: Colors.green, fontSize: 25.0),
+              child: Column(
+                children: <Widget>[
+                  new Stack(
+                    alignment: Alignment.topCenter,
+                    children: <Widget>[
+                      new Container(
+                        margin: new EdgeInsets.only(top:40.0),
+                        height: 80.0,
+                        width: 80.0,
+                        decoration: new BoxDecoration(
+                          borderRadius: new BorderRadius.circular(80.0),
+                          color: Color(0xFF18D191)
+                        ),
+                        child: new Icon(Icons.local_offer,color: Colors.white,),
                       ),
-                      new RaisedButton(
-                        child: new Text('Donation Page', style: new TextStyle(fontSize: 20.0)),
-                        onPressed: () => changePage(1)
+                      new Container(
+                        margin: new EdgeInsets.only(top:160.0),
+                        height: 80.0,
+                        width: 80.0,
+                        decoration: new BoxDecoration(
+                          borderRadius: new BorderRadius.circular(80.0),
+                          color: Color(0xFF0394fc)
+                        ),
+                        child: new Icon(Icons.fastfood,color: Colors.white,),
                       ),
-                      new RaisedButton(
-                        child: new Text('Yoink Page', style: new TextStyle(fontSize: 20.0)),
-                        onPressed: () => changePage(2)
+                      new Container(
+                        margin: new EdgeInsets.only(right: 110.0, top:100.0),
+                        height: 80.0,
+                        width: 80.0,
+                        decoration: new BoxDecoration(
+                          borderRadius: new BorderRadius.circular(80.0),
+                          color: Color(0xFFFC6A7F)
+                        ),
+                        child: new Icon(Icons.home,color: Colors.white,),
                       ),
-                      new RaisedButton(
-                        child: new Text('Maps Page', style: new TextStyle(fontSize: 20.0)),
-                        onPressed: () => changePage(3)
-                      ),
-                      new Padding(
-                         padding: EdgeInsets.only(top: 50.0)
+                      new Container(
+                        margin: new EdgeInsets.only(left: 110.0, top:100.0),
+                        height: 80.0,
+                        width: 80.0,
+                        decoration: new BoxDecoration(
+                          borderRadius: new BorderRadius.circular(80.0),
+                          color: Color(0xFFFFCE56)
+                        ),
+                        child: new Icon(Icons.directions_car,color: Colors.white,),
                       ),
                     ],
+                  ),
+                  new Container(
+                        child: Center(
+                            child: new Column(
+                              children: <Widget>[
+                                new Padding(padding: EdgeInsets.only(top: 80.0)),
+                                  new RaisedButton(
+                                    child: new Text('Donation Page', style: new TextStyle(fontSize: 20.0)),
+                                    onPressed: () => changePage(1)
+                                  ),
+                                  new RaisedButton(
+                                    child: new Text('Yoink Page', style: new TextStyle(fontSize: 20.0)),
+                                    onPressed: () => changePage(2)
+                                  ),
+                                  new RaisedButton(
+                                    child: new Text('Maps Page', style: new TextStyle(fontSize: 20.0)),
+                                    onPressed: () => changePage(3)
+                                  ),
+                                  new Padding(
+                                    padding: EdgeInsets.only(top: 50.0)
+                                  ),
+                                ],
+                              )
+                          ),
+                      ),
+                  new Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      new Text("iLeftover", style: new TextStyle(color: Colors.lightGreen, fontSize: 30.0), )
+                    ],
                   )
-              ),
-            )
+                ],
+              )
+            ),
           ),
         );
       case 1:
